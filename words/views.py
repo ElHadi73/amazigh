060187569503af9cd8a55e4266703d1d259fb13d
EL Hadi <gggg68131@gmail.com>
2020-12-26 22:07:27 +0100

    third commit developpement peroid

--- words/views.py
+++ words/views.py
@@ -4,7 +4,7 @@ from words.models import models
 from words.models.wordamazigh import Word
 
 # Create your views here.
-def home(request,word=None,input_lang=None,*args,**kwargs):
+def home(request,word=None,*args,**kwargs):
     output=[]
     context={}
     tit=""
@@ -12,7 +12,7 @@ def home(request,word=None,input_lang=None,*args,**kwargs):
         get=request.GET
         tit=get['search_text']
         output=models.search_result(get['input_lang'],get['output_lang'],get['search_text'])
-    elif word!=None and input_lang==None:
+    elif word!=None:
         tit=word
         output=models.search_result("Amazigh","Amazigh",word)
     context={"langs":Languages.in_langs(),'output':output,"tit":tit}
